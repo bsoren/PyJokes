12e6700fa6dd753ecc514c767ee3a094ac58a240
Bijay Soren <bijay.soren@gmail.com>
2025-10-22 14:36:09 +0530

    Using private members

--- pyjokes/joke_service.py
+++ pyjokes/joke_service.py
@@ -12,15 +12,15 @@ import os
 class JokeService:
     def __init__(self):
         load_dotenv()
-        self.to_emails = os.getenv("TO_EMAILS")
-        self.bcc_emails = os.getenv("BCC_EMAILS")
-        self.cc_emails = os.getenv("CC_EMAILS")
-        self.joke_api = "https://official-joke-api.appspot.com/jokes/random"
-        self.email_service = EmailService()
+        self.__to_emails = os.getenv("TO_EMAILS")
+        self.__bcc_emails = os.getenv("BCC_EMAILS")
+        self.__cc_emails = os.getenv("CC_EMAILS")
+        self.__joke_api = "https://official-joke-api.appspot.com/jokes/random"
+        self.__email_service = EmailService()
 
     def fetch_joke(self):
-        # TODO: fetch joke
-        response = requests.get(self.joke_api)
+        # fetching joke
+        response = requests.get(self.__joke_api)
         joke_dict = response.json()
         # print(joke_dict)
         return joke_dict
@@ -35,9 +35,9 @@ class JokeService:
         message = MIMEMultipart()
         # message["Resent-Date", datetime.now()]
         message["from"] = "bijay.soren@gmail.com"
-        message["to"] = self.to_emails
-        message["cc"] = self.cc_emails
-        message["bcc"] = self.bcc_emails
+        message["to"] = self.__to_emails
+        message["cc"] = self.__cc_emails
+        message["bcc"] = self.__bcc_emails
         message["subject"] = "Joke sent on " + self.get_unique_dt_str()
         message.attach(MIMEText(body, "html"))
 
@@ -46,11 +46,4 @@ class JokeService:
     def send_joke(self):
         joke = self.fetch_joke()
         message = self.build_email_message(joke)
-        self.email_service.send_email(message)
-
-
-if __name__ == "__main__":
-    js = JokeService()
-    print(f'Sending emails to {js.to_emails}')
-    js.send_joke()
-    print(f'Joke sent successfully on {js.get_unique_dt_str()}')
+        self.__email_service.send_email(message)
--- pyjokes/main.py
+++ pyjokes/main.py
@@ -2,6 +2,5 @@ from joke_service import JokeService
 
 if __name__ == "__main__":
     js = JokeService()
-    print(f'Sending emails to {js.to_emails}')
     js.send_joke()
     print(f'Joke sent successfully on {js.get_unique_dt_str()}')
